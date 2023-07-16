c6363b1ad992e3bc1aa90e593170a7258beeaf0f
sushblock <sushilstudy2013@gmail.com>
2023-07-16 19:22:11 +0530

    removed edge from middleware

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,6 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export const runtime = 'nodejs';
-export const preferredRegion = 'bom1';
-export const revalidate = 1200;
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req })
